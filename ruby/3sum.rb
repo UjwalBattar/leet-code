6a29b507a57bbcdb074507780e2cf86768363cea
Ujwal Battar <ujwal.battar@gmail.com>
2018-07-05 18:16:51 -0700

    three_sum ruby

--- ruby/3sum.rb
+++ ruby/3sum.rb
@@ -20,5 +20,33 @@
 # @return {Integer[][]}
 
 def three_sum(nums)
-
+  res = []
+  return res if nums.length < 3
+  nums = nums.sort
+  i = 0
+  while i < nums.length - 3
+    return res if nums[i] > 0
+    if i == 0 || nums[i] > nums[i - 1]
+      j = i + 1
+      k = nums.length - 1
+      while j < k
+        if nums[i] + nums[j] + nums[k] == 0
+          res << [nums[i], nums[j], nums[k]]
+        end
+        if nums[i] + nums[j] + nums[k] < 0
+          current_start = j
+          while nums[j] == nums[current_start] && j < k
+            j += 1
+          end
+        else
+          current_end = k
+          while nums[k] == nums[current_end] && j < k
+            k -= 1
+          end
+        end
+      end
+    end
+    i += 1
+  end
+  res
 end
