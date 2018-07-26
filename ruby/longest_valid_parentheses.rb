45eedd9da180493f3bcf5700ef8136f5c450b5fb
Ujwal Battar <ujwal.battar@gmail.com>
2018-07-25 21:23:35 -0700

    redo

--- ruby/longest_valid_parentheses.rb
+++ ruby/longest_valid_parentheses.rb
@@ -16,20 +16,5 @@
 # @param {String} s
 # @return {Integer}
 def longest_valid_parentheses(s)
-  count = 0
-  paren = []
-  i = 0
-  while i < s.length
-    if paren[-1] == s[i]
-      paren.pop
-    else
-      if s[i] == "("
-        paren.push(")")
-      else
-        paren.push("(")
-      end
-    end
-    i += 1
-  end
-  s.length - paren.length
+  
 end
