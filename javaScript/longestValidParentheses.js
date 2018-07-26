b96e21b7f49b979ddbd1bfb47af49ad0fc96fc3c
Ujwal Battar <ujwal.battar@gmail.com>
2018-07-26 12:37:07 -0700

    javascript

--- javaScript/longestValidParentheses.js
+++ javaScript/longestValidParentheses.js
@@ -17,26 +17,57 @@
  * @param {string} s
  * @return {number}
  */
+// var longestValidParentheses = function(s) {
+//   let maxLength = 0;
+//   let parenStack = [-1];
+//
+//   for (let i = 0; i < s.length; i++) {
+//     if (s[i] === "(") {
+//       parenStack.push(i);
+//     } else {
+//       parenStack.pop();
+//       if (parenStack.length === 0) {
+//         parenStack.push(i);
+//       } else {
+//         maxLength = Math.max(maxLength, i - parenStack[parenStack.length - 1]);
+//       }
+//     }
+//   }
+//   return maxLength;
+// };
 var longestValidParentheses = function(s) {
-  let count = 0;
-  const parenStack = [];
+  let lefts = 0;
+  let rights = 0;
+  let maxLen = 0;
 
   for (let i = 0; i < s.length; i++) {
-    if (parenStack[parenStack.length - 1] === s[i]) {
-      parenStack.pop();
-      count--;
+    if (s[i] === "(") {
+      lefts++;
     } else {
-      if ( s[i] === "(") {
-        parenStack.push(")");
-        count++;
-      } else if (s[i] === ")") {
-        parenStack.push("(");
-        count++;
-      }
+      rights++;
+    }
+
+    if (lefts === rights) {
+      maxLen = Math.max(maxLen, rights * 2);
+    } else if (rights >= lefts) {
+      lefts = rights = 0;
     }
   }
-  console.log("parenStack:", parenStack.length);
-  console.log("s:", s.length);
-  console.log("count:", count);
-  return s.length - parenStack.length;
+
+  lefts = rights = 0;
+  
+  for (let i = s.length - 1; i >= 0; i--) {
+    if (s[i] === '(') {
+      lefts++;
+    } else {
+      rights ++;
+    }
+
+    if (lefts === rights) {
+      maxLen = Math.max(maxLen, lefts * 2);
+    } else if (lefts >= rights) {
+      lefts = rights = 0;
+    }
+  }
+  return maxLen;
 };
