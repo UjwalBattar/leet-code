359b092c5f801d65e199f769ac7b8d1bbdab534b
Ujwal Battar <ujwal.battar@gmail.com>
2018-07-28 23:16:23 -0700

    merge k

---
+++ javaScript/mergeKLists.js
@@ -0,0 +1,46 @@
+// Merge k sorted linked lists and return it as one sorted list.
+// Analyze and describe its complexity.
+//
+// Example:
+//
+// Input:
+// [
+//   1->4->5,
+//   1->3->4,
+//   2->6
+// ]
+// Output: 1->1->2->3->4->4->5->6
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode[]} lists
+ * @return {ListNode}
+ */
+var mergeKLists = function(lists) {
+  if (lists.length === 0 ) return lists;
+
+  let mid = Math.floor(lists.length / 2);
+  let left = mergeKLists(lists.slice(0, mid));
+  let right = mergeKLists(lists.slice(mid));
+
+  return mergeTwoLists(left, right);
+};
+
+function mergeTwoLists(l1, l2) {
+  if (!l1) return l2;
+  if (!l2) return l1;
+
+  if (l1.val < l2.val) {
+    l1.next = mergeTwoLists(l1.next, l2);
+    return l1;
+  } else {
+    l2.next = mergeTwoLists(l1, l2.next);
+    return l2;
+  }
+}
---
+++ ruby/merge_k_lists.rb
@@ -0,0 +1,28 @@
+# Merge k sorted linked lists and return it as one sorted list.
+# Analyze and describe its complexity.
+#
+# Example:
+#
+# Input:
+# [
+#   1->4->5,
+#   1->3->4,
+#   2->6
+# ]
+# Output: 1->1->2->3->4->4->5->6
+
+# Definition for singly-linked list.
+# class ListNode
+#     attr_accessor :val, :next
+#     def initialize(val)
+#         @val = val
+#         @next = nil
+#     end
+# end
+
+# @param {ListNode[]} lists
+# @return {ListNode}
+
+def merge_k_lists(lists)
+
+end
