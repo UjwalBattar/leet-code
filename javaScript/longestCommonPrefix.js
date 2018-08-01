5d992405cff4a35fd0285e747d46c14b2240fd38
Ujwal Battar <ujwal.battar@gmail.com>
2018-07-31 20:37:48 -0700

    longest common prefix

--- javaScript/longestCommonPrefix.js
+++ javaScript/longestCommonPrefix.js
@@ -20,30 +20,61 @@
  * @return {string}
  */
 var longestCommonPrefix = function(strs) {
-  if (strs === null || strs.length === 0) return "";
+  if (strs.length === 0) return "";
   let minLength = Number.MAX_VALUE;
   for (let str of strs) {
     minLength = Math.min(minLength, str.length);
   }
-  let low = 1;
-  let high = minLength;
 
+  let low = 0;
+  let high = minLength;
   while (low <= high) {
-    let middle = (low + high) / 2;
+    let middle = Math.floor((low + high) / 2);
     if (isCommonPrefix(strs, middle)) {
       low = middle + 1;
     } else {
-      high = middle + 1;
+      high = middle - 1;
     }
   }
-  return strs[0].substring(0, (low + high) / 2);
+  return strs[0].substring(0, Math.floor((low + high) / 2));
 };
 
 function isCommonPrefix(strs, len) {
   let str1 = strs[0].substring(0, len);
+
   for (let i = 1; i < strs.length; i++) {
     if (!strs[i].startsWith(str1)) return false;
   }
-
   return true;
 }
+
+
+// var longestCommonPrefix = function(strs) {
+//   if (strs === null || strs.length === 0) return "";
+//   let minLength = Number.MAX_VALUE;
+//   for (let str of strs) {
+//     minLength = Math.min(minLength, str.length);
+//   }
+//   let low = 1;
+//   let high = minLength;
+//
+//   while (low <= high) {
+//     let middle = (low + high) / 2;
+//     if (isCommonPrefix(strs, middle)) {
+//       low = middle + 1;
+//     } else {
+//       high = middle + 1;
+//     }
+//   }
+//   return strs[0].substring(0, (low + high) / 2);
+// };
+//
+// function isCommonPrefix(strs, len) {
+//   let str1 = strs[0].substring(0, len);
+//   for (let i = 1; i < strs.length; i++) {
+//     if (!strs[i].startsWith(str1)) return false;
+//   }
+//
+//   return true;
+//
+// }
--- ruby/longest_common_prefix.rb
+++ ruby/longest_common_prefix.rb
@@ -19,7 +19,33 @@
 # @return {String}
 
 def longest_common_prefix(strs)
+  return "" if strs.length < 1
 
+  minLength = strs[0].length
+
+  strs.each {|str| minLength = [minLength, str.length].min}
+
+  low = 0
+  high = minLength
+
+  while low <= high
+    mid = (low + high) / 2
+    if is_common_substring(strs, len)
+      low = mid + 1
+    else
+      high = mid - 1
+    end
+  end
+  strs[0][0...((low + high) / 2)]
 end
 
-]
+def is_common_substring(strs, len)
+  str1 = strs[0][0...len]
+
+  i = 1
+  while i < strs.length
+    return false if strs[i][0...len] != str1
+    i += 1
+  end
+  true
+end
