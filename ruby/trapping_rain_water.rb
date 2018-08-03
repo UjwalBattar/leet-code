387ab9e549b42e143f0111710cfa5e55cf32df8f
Ujwal Battar <ujwal.battar@gmail.com>
2018-08-02 19:05:05 -0700

    trap rain ruby

--- ruby/trapping_rain_water.rb
+++ ruby/trapping_rain_water.rb
@@ -1,7 +1,6 @@
-# Given n non-negative integers representing an elevation map 
+# Given n non-negative integers representing an elevation map
 # where the width of each bar is 1, compute how much water
 # it is able to trap after raining.
-#
 #
 # The above elevation map is represented by array
 # [0,1,0,2,1,0,1,3,2,1,2,1].
@@ -9,6 +8,27 @@
 # trapped.
 #
 # Example:
-#
 # Input: [0,1,0,2,1,0,1,3,2,1,2,1]
 # Output: 6
+# @param {Integer[]} height
+# @return {Integer}
+
+def trap(height)
+  vol = 0
+  left = 0
+  right = height.length - 1
+  l_max = 0
+  r_max = 0
+
+  while left < right
+    if height[left] < height[right]
+      height[left] >= l_max ? l_max = height[left] : vol += height[left]
+      left += 1
+    else
+      height[right] >= r_max ? r_max = height[right] : vol += height[right]
+      right -= 1
+    end
+  end
+
+  vol
+end
