96543ce3528e1c0d44cf1e02a196bb9c563e0dca
Ujwal Battar <ujwal.battar@gmail.com>
2018-08-25 21:58:29 -0700

    swap pairs setup

---
+++ javaScript/swapNodeInPairs.js
@@ -0,0 +1,25 @@
+// Given a linked list, swap every two adjacent nodes and
+// return its head.
+//
+// Example:
+//
+// Given 1->2->3->4, you should return the list as 2->1->4->3.
+// Note:
+//
+// 1. Your algorithm should use only constant extra space.
+// 2. You may not modify the values in the list's nodes, only nodes
+//     itself may be changsd
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var swapPairs = function(head) {
+
+};
---
+++ ruby/swap_node_in_pairs.rb
@@ -0,0 +1,27 @@
+# Given a linked list, swap every two adjacent nodes and
+# return its head.
+#
+# Example:
+#
+# Given 1->2->3->4, you should return the list as 2->1->4->3.
+# Note:
+#
+# 1. Your algorithm should use only constant extra space.
+# 2. You may not modify the values in the list's nodes, only nodes
+#     itself may be changed
+
+# Definition for singly-linked list.
+# class ListNode
+#     attr_accessor :val, :next
+#     def initialize(val)
+#         @val = val
+#         @next = nil
+#     end
+# end
+
+# @param {ListNode} head
+# @return {ListNode}
+
+def swap_pairs(head)
+
+end
