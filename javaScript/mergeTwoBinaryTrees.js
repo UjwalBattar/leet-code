5cf868b4cfc3fc5a3e804adf385d998c6f3e0359
Ujwal Battar <ujwal.battar@gmail.com>
2018-09-04 15:57:38 -0700

    merge 2 binary trees ruby

--- javaScript/mergeTwoBinaryTrees.js
+++ javaScript/mergeTwoBinaryTrees.js
@@ -40,6 +40,6 @@ var mergeTrees = function(t1, t2) {
     if (!t2) return t1;
     t1.val += t2.val;
     t1.left = mergeTrees(t1.left, t2.left);
-    t2.right = mergeTrees(t1.right, t2.right);
+    t1.right = mergeTrees(t1.right, t2.right);
     return t1;
 };
--- ruby/merge_two_binary_trees.rb
+++ ruby/merge_two_binary_trees.rb
@@ -36,5 +36,10 @@
 # @return {TreeNode}
 
 def merge_trees(t1, t2)
-
+  return t2 if !t1
+  return t1 if !t2
+  t1.val += t2.val
+  t1.left = merge_trees(t1.left, t2.left)
+  t1.right = merge_trees(t1.right, t2.right)
+  t1
 end
