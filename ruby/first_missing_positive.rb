262e3b4b9da446d3b83386a7937ba3f8d4c134be
Ujwal Battar <ujwal.battar@gmail.com>
2018-09-09 21:42:06 -0700

    first missing positive ruby o(n) time o(n) space

--- ruby/first_missing_positive.rb
+++ ruby/first_missing_positive.rb
@@ -20,6 +20,38 @@
 # @param {Integer[]} nums
 # @return {Integer}
 
-def first_missing_positive(nums)
+# def first_missing_positive(nums)
+#   return 1 if nums.empty?
+#   i = 0
+#   while i < nums.length
+#     if nums[i] > 0 && nums[i] <= nums.length && nums[nums[i] - 1] != nums[i]
+#       a = nums[i]
+#       b = nums[nums[i] - 1]
+#       nums[nums[i] - 1], nums[i] = a, b
+#       p nums
+#     else
+#       i += 1
+#     end
+#   end
+#   i = 0
+#   while i < nums.length
+#     return i + 1 if nums[i] != i + 1
+#   end
+#   i + 1
+# end
 
+def first_missing_positive(nums)
+  arr = []
+  nums.each do |n|
+    arr[n - 1] = n if n > 0
+  end
+
+  i = 0
+  while i < arr.length
+    if arr[i] != i + 1
+      return i + 1
+    end
+    i += 1
+  end
+  return i
 end
