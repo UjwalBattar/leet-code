ff621d6f99f09b25ca940143108ba5235144e846
Ujwal Battar <ujwal.battar@gmail.com>
2018-09-13 12:32:34 -0700

    zero order array ruby

--- ruby/zero_order_array.rb
+++ ruby/zero_order_array.rb
@@ -7,5 +7,20 @@
 # Input: [0, 0, 2, 4, 6]
 # Output: [2, 4, 6, 0, 0]
 
+def zero_ord_arr(arr)
+  z = 0
+  i = 1
+  while i < arr.length
+    if arr[z] != 0 && arr[i] == 0
+      z = i
+    end
+    if arr[z] == 0 && arr[i] != 0
+      arr[z], arr[i] = arr[i], arr[z]
+      z += 1
+    end
+    i += 1
+  end
+  arr
+end
 
 zero_ord_arr([1, 0, 3, 0, 5])
