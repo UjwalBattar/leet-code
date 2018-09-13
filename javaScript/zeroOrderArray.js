d6013663ac86c352e1b00d3761bab35d7d84d534
Ujwal Battar <ujwal.battar@gmail.com>
2018-09-13 12:26:06 -0700

    zero ord array JS

--- javaScript/zeroOrderArray.js
+++ javaScript/zeroOrderArray.js
@@ -6,3 +6,18 @@
 //
 // Input: [0, 0, 2, 4, 6]
 // Output: [2, 4, 6, 0, 0]
+
+function zeroOrdArr(arr) {
+  let z = 0;
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[z] !== 0 && arr[i] === 0) {
+      z = i;
+    }
+
+    if (arr[i] !== 0) {
+      [arr[z], arr[i]] = [arr[i], arr[z]];
+      z++;
+    }
+  }
+  return arr;
+}
