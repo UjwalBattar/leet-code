3c3e8ee21d3cd7c90eba523f7625688606976656
Ujwal Battar <ujwal.battar@gmail.com>
2018-09-18 12:08:24 -0700

    do ruby later

--- ruby/sudoku_solver.rb
+++ ruby/sudoku_solver.rb
@@ -22,33 +22,30 @@
 # The given board size is always 9x9.
 # @param {Character[][]} board
 # @return {Void} Do not return anything, modify board in-place instead.
+# board =
+#    ["53..7....",
+#     "6..195...",
+#     ".98....6.",
+#     "8...6...3",
+#     "4..8.3..1",
+#     "7...2...6",
+#     ".6....28.",
+#     "...419..5",
+#     "....8..79"]
+#
+# puts sudoku(board).inspect
 
-BLANK = '.'
 
 def sudoku(board)
   # find first '.'
-  for i in 0...board.length
-    for j in 0...board[i].length
-      next unless board[i][j] == BLANK
-
-      options = get_options(board, i, j)
-      return nil if options.empty? # validity check failed, kill branch
+  # validity check failed, kill branch
 
       # put options on board individually, and recurse
-      proposed = board.dup
-      options.each do |v|
-        proposed[i][j] = v
-        return proposed if sudoku(proposed)
-        proposed[i][j] = BLANK
-      end
 
       # none of the options worked
-      return nil
-    end
-  end
 
   # base case: the whole board has numbers
-  board
+
 end
 
 def get_options(board, row, col)
@@ -58,48 +55,17 @@ def get_options(board, row, col)
   # - no repeated numbers in a column
   # - no repeated numbers in a block
 
-  options = [*"1".."9"] # all options
-  len = board.length
+  # all options
 
   # remove the options that exist in the same row
-  options -= board[row].chars.uniq
 
   # remove the options that exist in the same column
-  column = len.times.collect {|row| board[row][col]}
-  options -= column.uniq
 
   # remove the options that exist in the same block
-  block = get_block(board, row, col)
-  options -= block.uniq
 
   # remove blanks and return
-  options -= [BLANK]
-  options.map(&:to_s)
 end
 
 def get_block(board, row, col)
   # returns the entire block that the row/col falls in
-  row_start = 3 * (row / 3)
-  col_start = 3 * (col / 3)
-  block = []
-
-  for i in 0..2
-    for j in 0..2
-      block << board[row_start + i][col_start + j]
-    end
-  end
-  block
 end
-
-board =
-   ["53..7....",
-    "6..195...",
-    ".98....6.",
-    "8...6...3",
-    "4..8.3..1",
-    "7...2...6",
-    ".6....28.",
-    "...419..5",
-    "....8..79"]
-
-puts sudoku(board).inspect
