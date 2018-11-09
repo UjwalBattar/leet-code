578bea09d8fc10f3389b509879e6288a9d727c93
Ujwal Battar <ujwal.battar@gmail.com>
2018-11-08 20:11:12 -0800

    fix plus one JS, parseInt does not work with large numbers

--- javaScript/plusOne.js
+++ javaScript/plusOne.js
@@ -25,7 +25,23 @@
  * @param {number[]} digits
  * @return {number[]}
  */
+
 var plusOne = function(digits) {
   if (!digits.length) return [1];
-  return (parseInt(digits.join("")) + 1).toString().split("").map (el => parseInt(el));
+
+  digits[digits.length - 1] += 1;
+  for (var i = digits.length - 1; i > 0; i--) {
+      if (digits[i] >= 10) {
+          digits[i] = 0;
+          digits[i - 1] += 1;
+      } else {
+          break;
+      }
+  }
+  if (digits[0] === 10) {
+      digits[0] = 0;
+      digits.unshift(1);
+  }
+
+  return digits;
 };
