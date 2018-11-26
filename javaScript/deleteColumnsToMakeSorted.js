292a7cb1ee9bf61bc4618a3a0a3a6cf7ba189b3b
Ujwal Battar <ujwal.battar@gmail.com>
2018-11-25 17:20:42 -0800

    min deletion size javascript

--- javaScript/deleteColumnsToMakeSorted.js
+++ javaScript/deleteColumnsToMakeSorted.js
@@ -57,5 +57,16 @@
  * @return {number}
  */
 var minDeletionSize = function (A) {
-
+    let count = 0;
+    for (let i = 0; i < A[0].length; i++) {
+        for (let j = 0; j < A.length - 1; j++) {
+            if (A[j][i] > A[j + 1][i]) {
+                count++;
+                break;
+            }
+        }
+    }
+    return count;
 };
+
+minDeletionSize(["zyx", "wvu", "tsr"]);
