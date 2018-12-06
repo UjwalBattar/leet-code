bb75d7eeacddf1af854c0245d156ccc6ae8533b1
Ujwal Battar <ujwal.battar@gmail.com>
2018-12-06 10:57:04 -0800

    remove dups from sorted list JS

---
+++ ruby/remove_dups_from_sorted_list.rb
@@ -0,0 +1,26 @@
+# Given a sorted linked list, delete all duplicates such that 
+# each element appear only once.
+
+# Example 1:
+
+# Input: 1->1->2
+# Output: 1->2
+# Example 2:
+
+# Input: 1->1->2->3->3
+# Output: 1->2->3
+
+# Definition for singly-linked list.
+# class ListNode
+#     attr_accessor :val, :next
+#     def initialize(val)
+#         @val = val
+#         @next = nil
+#     end
+# end
+
+# @param {ListNode} head
+# @return {ListNode}
+def delete_duplicates(head)
+    
+end
